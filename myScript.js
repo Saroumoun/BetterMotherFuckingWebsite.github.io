87c229ffe7d6c1f5c663768a5639aa0f79738c5b
Sarou <bastien.hiel@gmail.com>
2019-01-02 18:13:01 +0100

    ajout fonctionnalité redirection

--- myScript.js
+++ myScript.js
@@ -1,3 +1,11 @@
 let monAlerte = 'Hey le site xxxvidsxxx est trop bien. Viens dessus stp please';
 
-setTimeout(function(){ alert(monAlerte); }, 10000);
+// setTimeout(function(){confirm(monAlerte)}, 10000);
+
+function rediriger (){
+	if(confirm(monAlerte)){
+		window.location = "https://www.youtube.com/watch?v=dQw4w9WgXcQ";
+	}
+}
+
+setTimeout(function(){rediriger()}, 10000);
