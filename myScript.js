ba18ad132fa4450dc0a7bb05d9bfd55dfc866bae
Sarou <bastien.hiel@gmail.com>
2019-01-02 19:01:26 +0100

    ajout fonction background magenta

--- myScript.js
+++ myScript.js
@@ -1,11 +1,13 @@
 let monAlerte = 'Hey le site xxxvidsxxx est trop bien. Viens dessus stp please';
 
-// setTimeout(function(){confirm(monAlerte)}, 10000);
-
-function rediriger (){
+function rediriger(){
 	if(confirm(monAlerte)){
 		window.location = "https://www.youtube.com/watch?v=dQw4w9WgXcQ";
 	}
 }
 
 setTimeout(function(){rediriger()}, 10000);
+
+function psychoBackground(){
+	document.querySelector('body').setAttribute("style", "background-color: magenta;");
+}
